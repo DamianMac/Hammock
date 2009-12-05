99c46c07832f0864f27f108cf4989eac1d3498a9
jorkul <jorkul@ff391d20-badf-11de-869a-473eb4776397>
2009-12-05 21:09:05 +0000

    switch to use the couch process
    git-svn-id: http://relax-net.googlecode.com/svn/trunk@27 ff391d20-badf-11de-869a-473eb4776397

--- RedBranch.Hammock.Test/ConnectionTests.cs
+++ RedBranch.Hammock.Test/ConnectionTests.cs
@@ -12,7 +12,7 @@ namespace RedBranch.Hammock.Test
     {
         public static Connection CreateConnection()
         {
-            return new Connection(new Uri("http://localhost:5984"));    
+            return new CouchProcess(new Uri("http://localhost:5984")).Connect();    
         }
 
         [TestFixtureSetUp]
