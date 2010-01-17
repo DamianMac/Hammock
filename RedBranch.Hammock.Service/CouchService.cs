46124a022b50d8d742949e4c056f6de9efca498e
nnystrom <nnystrom@ff391d20-badf-11de-869a-473eb4776397>
2010-01-17 23:09:39 +0000

    * Simple windows service to keep couch running.
    git-svn-id: http://relax-net.googlecode.com/svn/trunk@47 ff391d20-badf-11de-869a-473eb4776397

---
+++ RedBranch.Hammock.Service/CouchService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace RedBranch.Hammock.Service
+{
+    public partial class CouchService : ServiceBase
+    {
+        private Process _process;
+
+        public CouchService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _process = CouchProcess.EnsureRunning(new Uri("http://localhost:5984"));
+        }
+
+        protected override void OnStop()
+        {
+            if (null != _process)
+            {
+                _process.Kill();
+                _process = null;
+            }
+        }
+    }
+}
