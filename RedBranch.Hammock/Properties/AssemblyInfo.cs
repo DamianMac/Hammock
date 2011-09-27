061ccc98ecb5204fb629d7393343206cc5bf288c
nnystrom <nnystrom@ff391d20-badf-11de-869a-473eb4776397>
2011-09-27 17:09:13 +0000

    Updated assembly properties, added MonoDevelop packaging project. Released as 1.1.0.0.
    git-svn-id: http://relax-net.googlecode.com/svn/trunk@76 ff391d20-badf-11de-869a-473eb4776397

--- RedBranch.Hammock/Properties/AssemblyInfo.cs
+++ RedBranch.Hammock/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RedBranch.Hammock")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RedBranch")]
-[assembly: AssemblyProduct("RedBranch.Hammock")]
-[assembly: AssemblyCopyright("Copyright © Red Branch Software, Llc 2009")]
+[assembly: AssemblyCompany("Belay Software, LLC")]
+[assembly: AssemblyProduct("Hammock")]
+[assembly: AssemblyCopyright("Copyright © Belay Software, Llc 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
