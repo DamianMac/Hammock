fa4813caa01fc32b1dd4bda0956b2d7fc2d8b014
nnystrom@gmail.com <nnystrom@gmail.com@ff391d20-badf-11de-869a-473eb4776397>
2011-11-28 15:47:28 +0000

    Removed db DELETE retry code, as the Windows bug it was designed to cover for (http://issues.apache.org/jira/browse/COUCHDB-326) is supposedly not reproducable in 1.1.1 and later builds on Windows.
    git-svn-id: http://relax-net.googlecode.com/svn/trunk@82 ff391d20-badf-11de-869a-473eb4776397

--- RedBranch.Hammock/Connection.cs
+++ RedBranch.Hammock/Connection.cs
@@ -163,36 +163,6 @@ namespace RedBranch.Hammock
 
         public void DeleteDatabase(string database)
         {
-            // give a very short pause here, as there are some file locking issues
-            // in windows where the delete goes through before a previous file lock
-            // is released.
-            // http://issues.apache.org/jira/browse/COUCHDB-326
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT ||
-                Environment.OSVersion.Platform == PlatformID.Win32Windows)
-            {
-                for (int counter = 0; counter < 3; counter++)
-                {
-                    try
-                    {
-                        _DeleteDatabase(database);
-                        break;
-                    }
-                    catch (CouchException exception)
-                    {
-                        if (exception.Reason != "eacces")
-                            throw;
-                    }
-                }
-                System.Threading.Thread.Sleep(100);
-            }
-            else
-            {
-                _DeleteDatabase(database);
-            }
-        }
-
-        private void _DeleteDatabase(string database)
-        {
             var request = (HttpWebRequest)WebRequest.Create(GetDatabaseLocation(database));
             request.Method = "DELETE";
             request.GetCouchResponse().Close();
