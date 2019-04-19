ec15f4b2b28bc43135ca3745f1dc23ec673e5e76
damianmac <damian@damianm.com>
2019-04-19 15:24:20 +1000

    build number is env var

--- ci_build_test_publish.sh
+++ ci_build_test_publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose build --force-rm --build-arg BUILD_NUMBER
+docker-compose build --force-rm --build-arg $BUILD_NUMBER
 docker-compose up hammock-build && docker-compose up --exit-code-from hammock-test hammock-test
 if [ $? -eq 0 ]
 then
